3a8e3c765c5eecefec7886af0dc4f1650ad9721a
sophietk <sophie.tk@gmail.com>
2014-03-18 19:35:39 +0100

    Clean js code

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var SERVER_HOST = "http://127.0.0.1:3000",
-    ADMIN_PASS = "adminpass",
-    REFRESH_DELAY = 5 * 1000;
+var config = {
+    SERVER_HOST: "http://127.0.0.1:3000",
+    ADMIN_PASS: "adminpass",
+    REFRESH_DELAY: 5 * 1000
+};
 
 $(function() {
     chart = new Highcharts.Chart({
@@ -63,67 +65,13 @@ $(function() {
         }]
     });
 
-    $('input[name="server-host"]').blur(function() {
-        SERVER_HOST = $(this).val();
+    $('.title').click(function() {
+        $(this).siblings('.block').toggle('slide');
+        $(this).find('.arrow').toggleClass('collapsed');
     });
 
-    $('input[name="admin-pass"]').blur(function() {
-        ADMIN_PASS = $(this).val();
-    });
-
-    $('input[name="refresh-delay"]').blur(function() {
-        REFRESH_DELAY = $(this).val();
-    });
-
-    $('#options .title').click(function() {
-        $('#options .block').toggle('slide');
-        $('#options .arrow').toggleClass('collapsed');
-    });
-
-    $('button[data-action="new"]').click(function() {
-        $.ajax({
-            url: SERVER_HOST + '/admin/new',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify($('.game-size').val()),
-            headers: {
-                adminpass: ADMIN_PASS
-            },
-            success: function() {
-                $.bootstrapGrowl('New game started', { type: 'success' });
-            },
-            error: showErrorDialog
-        });
-    });
-
-    $('button[data-action="change-tempo"]').click(function() {
-        $.ajax({
-            url: SERVER_HOST + '/admin/tempo',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify($('.turn-tempo').val()),
-            headers: {
-                adminpass: ADMIN_PASS
-            },
-            success: function() {
-                $.bootstrapGrowl('Change turn temporizing successfully', { type: 'success' });
-            },
-            error: showErrorDialog
-        });
-    });
-
-    $('button[data-action="reset-scores"]').click(function() {
-        $.ajax({
-            url: SERVER_HOST + '/admin/reset',
-            type: 'POST',
-            headers: {
-                adminpass: ADMIN_PASS
-            },
-            success: function() {
-                $.bootstrapGrowl('Scores and games reset successfully', { type: 'success' });
-            },
-            error: showErrorDialog
-        });
+    $('#options input').blur(function() {
+        config[$(this).data('config')] = $(this).val();
     });
 
     $('button[data-action="reset-graph"]').click(function() {
@@ -139,20 +87,29 @@ $(function() {
         $.bootstrapGrowl('Graph reset', { type: 'success' });
     });
 
-    $('#logs .title').click(function() {
-        $('#logs .block').toggle('slide');
-        $('#logs .arrow').toggleClass('collapsed');
-    });
-
-    $('#grid .title').click(function() {
-        $('#grid .block').toggle('slide');
-        $('#grid .arrow').toggleClass('collapsed');
+    $('#admin button').click(function() {
+        $.ajax({
+            url: config.SERVER_HOST + $(this).data('action'),
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify($(this).parent().siblings('input').val()),
+            headers: {
+                adminpass: config.ADMIN_PASS
+            },
+            success: function() {
+                $.bootstrapGrowl('Admin change successful', { type: 'success' });
+            },
+            error: showErrorDialog
+        });
     });
 });
 
+var mergedLogs = [],
+    requestTimeout = null;
 function requestData() {
+    // Request scores
     $.ajax({
-        url: SERVER_HOST + '/scores',
+        url: config.SERVER_HOST + '/scores',
         success: function(scores) {
             _.each(scores, function(score, player) {
                 var serie = _.findWhere(chart.series, {name: player});
@@ -165,44 +122,39 @@ function requestData() {
                 }
                 serie.addPoint([new Date().getTime(), score], true);
             });
-
-            setTimeout(requestData, REFRESH_DELAY);
         },
-        error: function() {
-            setTimeout(requestData, REFRESH_DELAY);
-        }
+        error: showErrorDialog
     });
 
+    // Request game info
     $.ajax({
-        url: SERVER_HOST + '/admin/game',
+        url: config.SERVER_HOST + '/admin/game',
         headers: {
-            adminpass: ADMIN_PASS
+            adminpass: config.ADMIN_PASS
         },
         success: function(game) {
             var serie = _.findWhere(chart.series, {name: 'progress'});
             serie.addPoint([new Date().getTime(), game.progress], true);
-            if (!$('#grid .arrow').hasClass('collapsed')) {
-                var html = '';
-                _.each(game.grid, function(line) {
-                    html += '<tr>';
-                    _.each(line, function(square) {
-                        html += '<td class="' + color_class_map[square.color] + (square.found ? ' found"' : '"') + '>' + square.color + ' ' + square.symbol + '</td>';
-                    });
-                    html += '</tr>';
+
+            if ($('#grid .arrow').hasClass('collapsed')) return;
+            var html = '';
+            _.each(game.grid, function (line) {
+                html += '<tr>';
+                _.each(line, function (square) {
+                    html += '<td class="' + color_class_map[square.color] + (square.found ? ' found"' : '"') + '>' + square.color + ' ' + square.symbol + '</td>';
                 });
-                $('#grid table').html(html);
-            }
+                html += '</tr>';
+            });
+            $('#grid table').html(html);
         },
         error: showErrorDialog
     });
-}
 
-var mergedLogs = [];
-function refreshLogs() {
+    // Request logs
     $.ajax({
-        url: SERVER_HOST + '/admin/logs',
+        url: config.SERVER_HOST + '/admin/logs',
         headers: {
-            adminpass: ADMIN_PASS
+            adminpass: config.ADMIN_PASS
         },
         success: function(logs) {
             logs = logs.reverse();
@@ -222,9 +174,10 @@ function refreshLogs() {
         },
         error: showErrorDialog
     });
-    setTimeout(refreshLogs, REFRESH_DELAY);
+
+    clearTimeout(requestTimeout);
+    requestTimeout = setTimeout(requestData, config.REFRESH_DELAY);
 }
-refreshLogs();
 
 function showErrorDialog(data) {
     if (data.responseText) $.bootstrapGrowl(data.responseText, { type: 'danger' });
